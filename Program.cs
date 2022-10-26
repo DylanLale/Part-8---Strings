c71d97bde9cab2637ed90b85e5dfae0145a36d6d
DylanLale <disneygamer706@gmail.com>
2022-10-26 09:36:18 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System.ComponentModel.Design;
+
+namespace Part_8___Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int incorrect = 0;
+            bool done = false;
+            string guess;
+            while (!done) ;
+            Console.WriteLine("Welcome to Hangman");
+            Console.WriteLine("Enter a letter to guess");
+            guess = Console.ReadLine();
+        }
+        public static void DrawMan(int hanging)
+        {
+            if (hanging == 1)
+            {
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("=========");
+            }
+            else if (hanging == 2)
+            {
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("=========");
+            }
+            else if (hanging == 3)
+            {
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("=========");
+            }
+            else if (hanging == 4)
+            {
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine(" /|   |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("=========");
+            }
+            else if (hanging == 5)
+            {
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine(" /|\\  |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("=========");
+            }
+            else if (hanging == 6)
+            {
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine(" /|\\ |");
+                Console.WriteLine(" /    |");
+                Console.WriteLine("      |");
+                Console.WriteLine("=========");
+            }
+            else if (hanging == 7)
+            {
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("  O   |");
+                Console.WriteLine(" /|\\ |");
+                Console.WriteLine(" / \\ |");
+                Console.WriteLine("      |");
+                Console.WriteLine("=========");
+
+        }
+          
+            else if (hanging == 8)
+            {
+                Console.WriteLine("  +---+");
+                Console.WriteLine("  |   |");
+                Console.WriteLine("      |");
+                Console.WriteLine(" \\O/  |");
+                Console.WriteLine("  |   |");
+                Console.WriteLine(" / \\  |");
+                Console.WriteLine("=========");
+
+        }
+}        
+    }
+}
